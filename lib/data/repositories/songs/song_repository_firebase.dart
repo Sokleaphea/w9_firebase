6b08c66dc1a78176d6ed48ebafe5a9bb073c7035
phea <sokleapheaphon@gmail.com>
2026-03-25 11:54:37 +0700

    W9-01 - Fetch and display songs

---
+++ lib/data/repositories/songs/song_repository_firebase.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../model/songs/song.dart';
+import '../../dtos/song_dto.dart';
+import 'song_repository.dart';
+
+class SongRepositoryFirebase extends SongRepository {
+  static final Uri baseUri = Uri.https(
+    'w8-practice-53cd2-default-rtdb.asia-southeast1.firebasedatabase.app',
+  );
+  static final Uri songUri = baseUri.replace(path: 'songs.json');
+  // static final Uri artistUri = baseUri.replace(path: 'artists');
+
+  @override
+  Future<List<Song>> fetchSongs() async {
+    final http.Response response = await http.get(songUri);
+
+    if (response.statusCode == 200) {
+      // 1 - Send the retrieved list of songs
+      // List<dynamic> songJson = json.decode(response.body);
+      Map<String, dynamic> songJson = json.decode(response.body);
+      List<Song> result = [];
+      for (var iterable in songJson.entries) {
+        result.add(SongDto.fromJson(iterable.key, iterable.value));
+      }
+      return result;
+    } else {
+      // 2- Throw expcetion if any issue
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Future<Song?> fetchSongById(String id) async {}
+}
